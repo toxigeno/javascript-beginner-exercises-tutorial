c0f24fe64f15c1333199be27ba29780980f50cfe
toxigeno <melissaaraya@yahoo.com>
2021-02-07 06:50:29 +0000

    happy madrugada

--- exercises/13-How-much-the-wedding-costs/app.js
+++ exercises/13-How-much-the-wedding-costs/app.js
@@ -4,18 +4,16 @@ function getPrice(guests){
     let cost = 0;
     // Your code here
      if (guests > 200) {
-        cost = '$20,000';
+        cost = 20000;
+    } else if (guests > 100) {
+        cost = 15000;
+    } else if (guests > 50) {
+        cost = 10000;
+    } else {
+       cost = 4000;
     }
-    else if (guest > 100) {
-        cost = '$15,000';
-    }
-
-    else if (guests > 50){
-        cost = '10,000';
-    }
-    else (cost = '4,000');
     return cost;
 }
 
-let price = getPrice(input);
+let price = getPrice(guests);
 console.log('Your wedding will cost '+price+' dollars');

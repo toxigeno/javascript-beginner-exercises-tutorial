bd9868da38d3f6c8feab8a8b6f0703884ca00f63
toxigeno <melissaaraya@yahoo.com>
2021-02-07 02:58:29 +0000

    ahi vamos

--- exercises/13-How-much-the-wedding-costs/app.js
+++ exercises/13-How-much-the-wedding-costs/app.js
@@ -3,8 +3,21 @@ let guests = prompt('How many people are coming to your wedding?');
 function getPrice(guests){
     let cost = 0;
     // Your code here
+    
+    if (guests <= 50) {
+        cost = "$4,000";
+    }
+    else if (guests <= 100) {
+        cost = "10,000";
+    }
+    else if (guests <= 200){
+        cost = "15,000";
+    }
+    else (guest => 200){
+        cost = "20,000";
+    }
     return cost;
 }
 
 let price = getPrice(input);
-console.log('Your wedding will cost '+price+' dollars');
+console.log('Your wedding will cost '+Price+' dollars');

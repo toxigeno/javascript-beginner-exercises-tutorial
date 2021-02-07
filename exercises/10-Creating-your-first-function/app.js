19fa492e54d3e9fd40063b4ac643384c5c5bcb83
toxigeno <melissaaraya@yahoo.com>
2021-02-07 01:32:08 +0000

    mejor

--- exercises/10-Creating-your-first-function/app.js
+++ exercises/10-Creating-your-first-function/app.js
@@ -1,7 +1,8 @@
 function addNumbers(a,b)
 {
-	// This is the function body. Write your code here.
+    // This is the function body. Write your code here.
+    return a + b
 }
-
+var sum = addNumbers;
 //Do not change the code below
 console.log(addNumbers(3,4));

ac902c350e2ef396dfdef00fd8ad9809b616f7b1
toxigeno <melissaaraya@yahoo.com>
2021-02-07 02:25:06 +0000

    mejor

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,15 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+
+  if (total >= 100) {
+     alert = "Are we there yet?";
+  } 
+ else if (total > 50){
+    alert = "We'll be there in 5 minutes";
+  }
+  if (total <= 50){
+    alert = "I'm parking, I see you right now";
+  }
+
+console.log(alert);

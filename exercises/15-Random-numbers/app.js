9382b17562921a07415394d290d63da8dbdd31c1
toxigeno <melissaaraya@yahoo.com>
2021-02-08 05:29:56 +0000

    15 done

--- exercises/15-Random-numbers/app.js
+++ exercises/15-Random-numbers/app.js
@@ -1,10 +1,10 @@
 /*Exercise closely resembles 11 - consider introducing another concept here? */
 
-function getRandomInt()
-{
-	var randomNumber = Math.random();
-	return randomNumber;
-}
+ function getRandomInt(randomNumber)
+ {
+ 	var randomNumber = Math.random();
+ 	return Math.floor(Math.random() * Math.floor(1 * 10));
+ }
 
+ console.log(getRandomInt(1,10));
 
-console.log(getRandomInt());
